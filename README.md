cab828f3a89dc56d8e20415fcd768c2dabd5b847
shinshin86 <beagles1986@gmail.com>
2023-01-14 14:01:08 +0900

    Add suspend and resume and concurrency

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ Using OCR, move only the photos that contain recognizable characters to a separa
 ## Usage
 
 ```sh
-# python main {target dir} {language} {Destination document path}
-python main.py . jpn ./doc-files
+# python main {target dir} {language} {Destination document path} {If exists specify a saved file path of already checked files}
+python main.py . jpn ./doc-files documents-salvage-ship_already-checked-files.txt
 ```
+
+## Suspend and Resume
+
+If you interrupt the process with Ctrl + C, a file named `documents-salvage-ship_already-checked-files.txt` will be created at the current location of the path you executed.  
+Here you will find a comma-separated list of files that have already been checked.  
+If you want to skip checking files that have already been checked next time, you must specify the path to this file as well.
--- main.py
+++ main.py
@@ -8,13 +8,20 @@ import os
 from os import path
 import re
 import shutil
+import asyncio
+import functools
+import threading
 
+
+# global vars
+checked_files = []
+already_checked_files = []
 
 # support image files
 pattern = ".*\.(jpg|jpeg|png|gif|bmp)"
 
 
-def is_doc_image(img, lang):
+def is_doc_image(img, lang, tool):
     try:
         image = Image.open(img)
     except FileNotFoundError:
@@ -38,12 +45,10 @@ def is_doc_image(img, lang):
         return False
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Document image search")
-    parser.add_argument("dir", metavar="dir", type=pathlib.Path, help="Target dir path")
-    parser.add_argument("lang", metavar="lang", help="Language")
-    parser.add_argument("mvdir", metavar="mvdir", type=pathlib.Path, help="Destination path of the document")
-    args = parser.parse_args()
+async def main(dir, lang, mvdir, checkedfiles):
+    lock = threading.Lock()
+
+    os.makedirs(mvdir, exist_ok=True)
 
     tools = pyocr.get_available_tools()
     if len(tools) == 0:
@@ -53,16 +58,60 @@ if __name__ == "__main__":
     tool = tools[0]
     print("Using OCR Tool: '%s'" % (tool.get_name()))
 
-    image_files = [f for f in os.listdir(path=args.dir) if re.search(pattern, f, re.IGNORECASE)]
+    image_files = [f for f in os.listdir(path=dir) if re.search(pattern, f, re.IGNORECASE)]
     print("check file count: ", len(image_files))
 
     document_files = []
-    for f in image_files:
-        print("check file: ", f)
-        if is_doc_image(path.abspath(f), args.lang) is True:
-            document_files.append(path.abspath(f))
 
-    os.makedirs(args.mvdir, exist_ok=True)
-    for doc in document_files:
-        result = shutil.move(doc, path.abspath(args.mvdir))
-        print("move document file: ", doc, " => ", result)
+    loop = asyncio.get_event_loop()
+
+    if checkedfiles is not None:
+        with open(path.abspath(checkedfiles)) as file:
+            already_checked_files = file.read().split(',')
+        print("Exists already checked files: ", already_checked_files)
+    
+    for f in image_files:
+        if checkedfiles is not None:
+            if f in already_checked_files:
+                continue
+
+        print("check file: ", f)
+        asbpath = path.abspath(f)
+        is_doc = await loop.run_in_executor(None, functools.partial(
+            is_doc_image, asbpath, lang, tool
+        ))
+
+        with lock:
+            if is_doc is True:
+                result = shutil.move(path.abspath(f), path.abspath(mvdir))
+                print("move document file: ", path.abspath(f), " => ", result)
+            checked_files.append(f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Document image search")
+    parser.add_argument("dir", metavar="dir", type=pathlib.Path, help="Target dir path")
+    parser.add_argument("lang", metavar="lang", help="Language")
+    parser.add_argument("mvdir", metavar="mvdir", type=pathlib.Path, help="Destination path of the document")
+    parser.add_argument("checkedfiles", metavar="checkedfiles", type=pathlib.Path, default=None, nargs='?', help="Path to text file with list of files already checked")
+    args = parser.parse_args()
+
+    try:
+        while True:
+            asyncio.run(main(args.dir, args.lang, args.mvdir, args.checkedfiles))
+    except KeyboardInterrupt:
+        print("Quit salvage!")
+        print("Save checked files...")
+        save_text = ""
+        if args.checkedfiles is not None:
+            with open(path.abspath(args.checkedfiles)) as file:
+                save_text += file.read().rstrip()
+
+        if save_text != "":
+            save_text += ","
+
+        save_text +=  ",".join(checked_files)
+        save_file_name = "documents-salvage-ship_already-checked-files.txt"
+        with open(save_file_name, "w") as file:
+            file.write(save_text)
+            print("saved file:", save_file_name)
